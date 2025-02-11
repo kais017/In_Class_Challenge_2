060aa9fc344e5ec554be3519828e2d4b83c4253d
Your Name <your.email@example.com>
2025-02-11 16:06:19 -0500

    Completed task 1

--- cc_example.js
+++ cc_example.js
@@ -1,8 +1,16 @@
 // Task 1 
 
 let products = [
-{ name: "Laptop", price: 1200, category: "Electronics"},
-{ }
+{ name: "Laptop", price: 1200, category: "Electronics"}, // T
+{ name: "Notebook", price: 5, category: "Stationary"}, //F
+{ name: "Headphones", price: 100, category: "Electronics"} //T
+
+];
 
 
-]
+function getProductsByCategory(products, category)
+ { return products.filter(product => product.category === category);
+
+ }
+
+ console.log(getProductsByCategory(products, "Electronics"));
