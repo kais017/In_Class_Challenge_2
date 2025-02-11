d61950ac7d3337044bfd7d8d21ab15854857d61f
Your Name <your.email@example.com>
2025-02-11 16:27:31 -0500

    completed task 2

--- cc_example.js
+++ cc_example.js
@@ -12,5 +12,25 @@ function getProductsByCategory(products, category)
  { return products.filter(product => product.category === category);
 
  }
-
  console.log(getProductsByCategory(products, "Electronics"));
+
+ // Task 2: array methods - map 
+
+ console.log(products)
+ console.log("*".repeat(20))
+
+
+ function applyDiscount(products, discountRate) {
+     return products.map( product =>({
+        ...product,
+        price: product.price - (product.price * discountRate)
+
+    }
+
+    ))
+    }
+
+console.log(applyDiscount(products, 0.1)); 
+console.log(products)
+console.log("*".repeat(20))
+
